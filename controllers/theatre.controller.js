90b934c6dab756bc57f92fd3e49c87564a9b7e40
tarunbapat <tarun.bapat710@gmail.com>
2024-11-29 22:28:28 +0530

    theatre controllers

--- controllers/theatre.controller.js
+++ controllers/theatre.controller.js
@@ -1,13 +1,12 @@
 import Theatre from "../models/theatreSchema.model.js";
 
-export const createTheatre = async (req, res) => {
+const createTheatre = async (req, res) => {
   const theatre = req.body;
   const user = req.user;
   const theatreData = {
     ...theatre,
     owner: user.id,
   };
-  console.log("theatreData", theatreData);
   try {
     const createdTheatre = await Theatre.create(theatreData);
     return res.status(200).send({
@@ -17,3 +16,38 @@ export const createTheatre = async (req, res) => {
     return res.status(401).send({ message: "error while creating theatre" });
   }
 };
+
+const deleteTheatre = async (req, res) => {
+  try {
+    console.log("req.params.id", req.params.id);
+    await Theatre.deleteOne({ _id: req.params.id });
+    return res.status(200).send({ message: true });
+  } catch (error) {
+    return res.status(401).send({ message: "error while deleting theatre" });
+  }
+};
+
+const updateTheatre = async (req, res) => {
+  // console.log("req param from update", req);
+  try {
+    const theatre = await Theatre.updateOne(
+      { _id: req.params.id },
+      { $set: { ...req.body } }
+    );
+
+    return res.status(200).send({ theatre });
+  } catch (error) {
+    return res.status(401).send({ message: "error while updating theatre" });
+  }
+};
+
+const getTheatreById = async (req, res) => {
+  try {
+    const theatre = await Theatre.find({ _id: req.params.id });
+    return res.status(200).send({ theatre });
+  } catch (error) {
+    return res.status(401).send({ message: "error while fetching theatre" });
+  }
+};
+
+export { updateTheatre, deleteTheatre, createTheatre, getTheatreById };
--- routes/theatre.route.js
+++ routes/theatre.route.js
@@ -1,9 +1,17 @@
 import express from "express";
-import { createTheatre } from "../controllers/theatre.controller.js";
+import {
+  updateTheatre,
+  deleteTheatre,
+  createTheatre,
+  getTheatreById,
+} from "../controllers/theatre.controller.js";
 import Auth from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/create", Auth, createTheatre);
+router.post("/:id", Auth, updateTheatre);
+router.delete("/:id", Auth, deleteTheatre);
+router.put("/:id", getTheatreById);
 
 export default router;
